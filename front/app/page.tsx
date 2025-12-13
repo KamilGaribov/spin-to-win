1c4fd288a912a7bf59855d270f31b6de128d567f
Kamil <kamilgarib@gmail.com>
2025-12-13 18:08:19 +0400

    suspense

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -10,12 +10,12 @@ export default function PosterPage() {
   const posterNumber = searchParams.get("poster") ?? "";
 
   return (
-    <div className="w-1/2 h-1/2 bg-blue-400 m-auto mt-20">
-      <h1>Poster Page</h1>
-      <p>Poster number: {posterNumber}</p>
-      <Suspense fallback={<div>Loading form...</div>}>
+    <Suspense fallback={<div>Loading form...</div>}>
+      <div className="w-1/2 h-1/2 bg-blue-400 m-auto mt-20">
+        <h1>Poster Page</h1>
+        <p>Poster number: {posterNumber}</p>
         <SpinForm posterNumber={posterNumber} />
-      </Suspense>
-    </div>
+      </div>
+    </Suspense>
   );
 }
