35e92a8e2133fdf5b592e464ebb8cc053a0d23e7
Kamil <kamilgarib@gmail.com>
2025-12-13 19:42:19 +0400

    suspense

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,13 +1,14 @@
 import SpinForm from "./components/form";
 
 type PageProps = {
-  searchParams: {
+  searchParams: Promise<{
     poster?: string;
-  };
+  }>;
 };
 
-export default function PosterPage({ searchParams }: PageProps) {
-  const posterNumber = searchParams.poster ?? "";
+export default async function PosterPage({ searchParams }: PageProps) {
+  const { poster } = await searchParams;
+  const posterNumber = poster ?? "";
 
   return (
     <div className="w-1/2 h-1/2 bg-blue-400 m-auto mt-20">
