c16fed7def6b8217b524a10f5adabe06c34353f0
Kamil <kamilgarib@gmail.com>
2025-12-13 19:12:36 +0400

    suspense

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-// import { useSearchParams } from "next/navigation";
 import SpinForm from "./components/form";
-import { Suspense } from "react";
-
 
 export default function PosterPage() {
-  // const searchParams = useSearchParams();
-  // const posterNumber = searchParams.get("poster") ?? "";
-
   return (
-    <Suspense fallback={<div>Loading form...</div>}>
-      <div className="w-1/2 h-1/2 bg-blue-400 m-auto mt-20">
-        <h1>Poster Page</h1>
-        <SpinForm />
-      </div>
-    </Suspense>
+    <div className="w-1/2 h-1/2 bg-blue-400 m-auto mt-20">
+      <h1>Poster Page</h1>
+      <SpinForm />
+    </div>
   );
 }
